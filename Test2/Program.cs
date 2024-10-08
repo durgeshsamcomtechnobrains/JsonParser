51a4f10cb8d15ed587bbe423b30a39f1d053667d
Durgesh Marathe <durgesh.m@samcomtechnobrains.com>
2024-10-08 17:43:54 +0530

    Test2 1st commit

---
+++ Test2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\DotNetTeamBackup\Durgesh\MAUI Learning\JSON_Parser\JsonParser\Test2\test.json";
+            string json;
+            json = File.ReadAllText(path);
+            Console.WriteLine("JSON Content:");
+            Console.WriteLine(json);
+
+        }        
+    }
+}
